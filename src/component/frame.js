bb169ef19b00d2e456a18d055c60d86bdd02c6cb
ksks <nose2002kr@naver.com>
2024-06-02 16:11:43 +0900

    edit frame appearance

--- src/component/frame.js
+++ src/component/frame.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Tooltip from '@mui/material/Tooltip';
 
 import './frame.css';
-import { useAuth } from '../context/AuthContext';
 import Video from './videos'
+import Server from './servers'
 import Markdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
 import { useCard } from '../context/CardContext';
@@ -28,19 +28,8 @@ const MarkdownRenderer = ({ markdown }) => (
   
 const Frame = () => {
     const [activeContent, setActiveContent] = useState('whoiam');
-    const [serverStatus, setServerStatus] = useState({
-        server1: true,
-        server2: true,
-    });
     const showContent = (id) => {
         setActiveContent(id);
-    };
-
-    const toggleServerStatus = (server) => {
-        setServerStatus((prevStatus) => ({
-            ...prevStatus,
-            [server]: !prevStatus[server],
-        }));
     };
 
     const { setPhase } = useCard();
@@ -69,16 +58,16 @@ const Frame = () => {
         downloadFile();
       }, []);
 
-
-    const {isAuthenticated} = useAuth();
-
     const publicUrl = process.env.PUBLIC_URL;
 
     return (
         <div className="text-gray-800 height-100 container" style={{opacity:"0"}}>
             <div className="mx-auto height-100">
-                <header className="flex items-center justify-between p-4 py-4 header">
-                    <div className="text-3xl font-bold ml-1">KSKS</div>
+                <header className="flex items-center justify-between p-4 py-4 header" style={{ backgroundImage: 'url(' + `${publicUrl}/image.png` + ')', backgroundSize: 'cover', backgroundRepeat:'no-repeat' }}>
+                    <div></div>
+                    <img src="https://avatars.githubusercontent.com/u/52348275?v=4" className="avatar avatar-user width-full border color-bg-default"
+                    style={{ marginLeft: '60pt', marginBottom: '40pt', width:'auto', height:'100%'}} onClick={()=>window.location.href='https://github.com/nose2002kr'}/>
+                    {/* <div className="text-3xl font-bold ml-1">KSKS</div> */}
                     <div className="height-100"><img className="logo" src={`${publicUrl}/character.webp`} alt='' onClick={gotoLogin}/></div>
                 </header>
                 <div className="flex height-80 shadow-2">
@@ -89,7 +78,7 @@ const Frame = () => {
                                 className="py-2 px-4 cursor-pointer hover:bg-gray-100 category"
                                 onClick={() => showContent('whoiam')}
                             >
-                            <b>Who I am</b>
+                            <b>About me</b>
                             </li>
                             <li
                                 className="py-2 px-4 cursor-pointer hover:bg-gray-100 category"
@@ -97,14 +86,12 @@ const Frame = () => {
                             >
                                 <b>Projects</b>
                             </li>
-                            {isAuthenticated && (
                             <li
                                 className="py-2 px-4 cursor-pointer hover:bg-gray-100 category"
                                 onClick={() => showContent('servers')}
                             >
                                 <b>Servers</b>
                             </li>
-                            )}
                         </ul>
                     </nav>
                     <main className="w-3/4 p-4 overflow-scroll">
@@ -116,30 +103,9 @@ const Frame = () => {
                         {activeContent === 'video' && (
                             <Video></Video>
                         )}
-                        {
-                        isAuthenticated &&
-                        activeContent === 'servers' && (
+                        {activeContent === 'servers' && (
                             <div id="servers" className="content">
-                                <ul>
-                                    <li
-                                        className="py-2 flex items-center cursor-pointer"
-                                        onClick={() => toggleServerStatus('server1')}
-                                    >
-                                        <span
-                                            className={`w-3 h-3 rounded-full mr-2 ${serverStatus.server1 ? 'bg-green-500' : 'bg-red-500'}`}
-                                        ></span>
-                                        Server 1
-                                    </li>
-                                    <li
-                                        className="py-2 flex items-center cursor-pointer"
-                                        onClick={() => toggleServerStatus('server2')}
-                                    >
-                                        <span
-                                            className={`w-3 h-3 rounded-full mr-2 ${serverStatus.server2 ? 'bg-green-500' : 'bg-red-500'}`}
-                                        ></span>
-                                        Server 2
-                                    </li>
-                                </ul>
+                                <Server></Server>
                             </div>
                         )}
                     </main>
